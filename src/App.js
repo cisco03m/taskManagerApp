eb5b33b5538dfc73993bac67d66f186fa637bc02
Cagle19 <108105279+Cagle19@users.noreply.github.com>
2024-11-22 20:41:15 -0500

    Initial commit for new repository

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import TaskInput from './TaskInput';
+import './App.css';
+function App(){
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+
+  };
+  const deleteTask = (indexToDelete) =>{
+    setTasks(tasks.filter((_,index)=> index !==indexToDelete));
+}
+
+ return(
+  <div className='App'>
+      <h1>Task Manager</h1>
+      <TaskInput handleAddTask={addTask} />
+      <ul>
+        {tasks.map((task,index)=>(
+          <li key={index}>
+            {task}
+            <button onClick={()=>deleteTask(index)}>Delete</button>
+          </li>
+      )) }
+    </ul>
+  </div>
+
+ );
+}
+export default App;
